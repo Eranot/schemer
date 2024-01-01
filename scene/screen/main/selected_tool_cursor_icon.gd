5c8fb2190e28a26c9b5fd69a02651972e005554e
Renato MK <renato.konflanz@unochapeco.edu.br>
2024-01-01 16:54:10 -0300

    Adding mouse icon to show which tool is selected

---
+++ scene/screen/main/selected_tool_cursor_icon.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var selected_tool_cursor_icon = %SelectedToolCursorIcon
+
+
+func _ready():
+	GlobalEvents.select_tool.connect(on_select_tool)
+	hide()
+
+
+func _process(delta):
+	global_position = get_global_mouse_position() + Vector2(10, 10)
+
+
+func on_select_tool(tool: Enums.TOOL):
+	show()
+	
+	if tool == Enums.TOOL.CREATE_TABLE:
+		selected_tool_cursor_icon.texture = preload("res://asset/image/create_table_icon_normal.png")
+	elif tool == Enums.TOOL.ONE_TO_MANY:
+		selected_tool_cursor_icon.texture = preload("res://asset/image/one_to_many_icon_normal.png")
+	elif tool == Enums.TOOL.MANY_TO_MANY:
+		selected_tool_cursor_icon.texture = preload("res://asset/image/many_to_many_icon_normal.png")
+	else:
+		hide()
